c40925b804cc5ac459068f4c71953c7945aab65f
AhmedMahmoudAhmedMahmoud <130584964+AhmedMahmoudAhmedMahmoud@users.noreply.github.com>
2023-05-10 04:35:55 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -247,14 +247,14 @@ int main ()
   **/
   PID pid_steer = PID();
 
-  pid_steer.Init(0.3, 0.0007, 0.3, 1.2, -1.2);
+  pid_steer.Init(0.48, 0.0008, 0.4, 1.2, -1.2);
 
   // initialize pid throttle
   /**
   * TODO (Step 1): create pid (pid_throttle) for throttle command and initialize values
   **/
   PID pid_throttle = PID();
-  pid_throttle.Init(0.2, 0.0009, 0.1, 1.0, -1.0);
+  pid_throttle.Init(0.2, 0.00087, 0.12, 1.0, -1.0);
 
   h.onMessage([&pid_steer, &pid_throttle, &new_delta_time, &timer, &prev_timer, &i, &prev_timer](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode)
   {
